a44ea5842bc845f45656562cdcaae8a9a7a1f12e
hashish <Mhashish@smart-medicalservices.com>
2018-08-14 02:54:36 +0200

    add 404 interceptor

--- src/app/services/interceptors/error-interceptor.service.ts
+++ src/app/services/interceptors/error-interceptor.service.ts
@@ -6,11 +6,13 @@ import {Observable, throwError} from "rxjs";
 import {AuthService} from "../auth-service.service";
 import {HttpHandler} from "@angular/common/http";
 import {catchError} from "rxjs/internal/operators";
+import {NotificationsService} from 'angular2-notifications';
+import {Router} from '@angular/router';
 
 @Injectable()
 export class ErrorInterceptorService implements HttpInterceptor {
 
-  constructor(public authService : AuthService) { }
+  constructor(public authService : AuthService , public notificationService : NotificationsService , public router : Router) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
@@ -19,6 +21,11 @@ export class ErrorInterceptorService implements HttpInterceptor {
         // auto logout if 401 response returned from api
           localStorage.clear();
         // location.reload(true);
+      }
+      if(err.status === 404){
+        this.notificationService.error('خطأ 404', 'لا يوجد هذا ال url',{timeOut:3000})
+        this.router.navigate(['']);
+
       }
 
       // const error = err.error.message || err.statusText;
