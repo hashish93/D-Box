01aa2f58079b20dcd148035dfe051d93f2678a1b
hashish <Mhashish@smart-medicalservices.com>
2018-08-18 14:38:04 +0200

    edits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first app';
+  public scroll;
+
+  @HostListener('window:scroll', ['$event']) onScrollEvent($event){
+    const verticalOffset = window.pageYOffset
+      || document.documentElement.scrollTop
+      || document.body.scrollTop || 0;
+    console.log(verticalOffset);
+    this.scroll = verticalOffset;
+  }
 }
