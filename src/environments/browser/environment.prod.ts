30b0079b887fbc1a7490b33bd664617d665bf818
hashish <Mhashish@smart-medicalservices.com>
2018-10-06 12:35:49 +0200

    edits

--- src/environments/browser/environment.prod.ts
+++ src/environments/browser/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
     apiEndpoint: 'https://piksels-api.n-stream.tv/api/1.0/portal/',
     staticFilesEndPoint: 'https://piksels-api.n-stream.tv/',
     videoEndPoint: 'https://piksels-backend.n-stream.tv/v/',
+    shareEndPoint: 'http://piksels.n-stream.tv/',
     production: true,
     isServer: false,
     // for prerender
