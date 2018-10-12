ff02a43d9f23d53b8079d780c4a6ef8f488d0719
hashish <Mhashish@smart-medicalservices.com>
2018-10-12 14:00:56 +0200

    edits in url

--- src/app/download-apps/download-apps.component.ts
+++ src/app/download-apps/download-apps.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {CookieService} from 'ngx-cookie-service';
 import {Router} from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-download-apps',
@@ -9,7 +10,7 @@ import {Router} from '@angular/router';
 })
 export class DownloadAppsComponent implements OnInit {
 
-  constructor(public cookieService: CookieService, public router: Router) {
+  constructor(public cookieService: CookieService, public router: Router , private location : Location) {
   }
 
 
@@ -24,7 +25,7 @@ export class DownloadAppsComponent implements OnInit {
     const mobile_redirect = this.cookieService.get('mobile_redirect');
     if (mobile_redirect && mobile_redirect === 'true') {
       this.cookieService.set('mobile_redirect', 'false', 1);
-      this.router.navigate(['']);
+      this.location.back();
     }
   }
 
